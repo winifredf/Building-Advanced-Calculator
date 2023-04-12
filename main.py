9c60ff0dc72cc330190a77d90e518c0583b4cf3b
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-11 23:15:45 -0400

    Created if statements for operators

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ num1 = float(input("Enter first numer: "))
 op = input("Enter operator: "))
 num2 = float(input("Enter third number: "))
 
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op = "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
+
