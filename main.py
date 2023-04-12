9c68f57911c1e12a54c4dd0ca3e6864c171360fc
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-11 23:12:15 -0400

    inserted op variable btwn num1 and num2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 num1 = float(input("Enter first numer: "))
-num2 = float(input("Enter second numer: "))
-num3 = float(input("Enter third number: "))
+op = input("Enter operator: "))
+num2 = float(input("Enter third number: "))
 
